541b999d7cb32405d282725f5f7431861e551266
Tharindu37 <tharindut520@gmail.com>
2022-09-19 04:32:59 +0530

    Add modelmapper dependency

--- src/main/java/lk/ijse/pos/thogakadeapispring/config/ProjectConfig.java
+++ src/main/java/lk/ijse/pos/thogakadeapispring/config/ProjectConfig.java
@@ -1,17 +1,10 @@
-//package lk.ijse.pos.thogakadeapispring.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@Configuration
-//public class ProjectConfig {
-//
+package lk.ijse.pos.thogakadeapispring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
 //    @Bean
 //    public UserDetailsService userDetailsService() {
 //        InMemoryUserDetailsManager uds=new InMemoryUserDetailsManager();
@@ -25,4 +18,7 @@
 //    public PasswordEncoder passwordEncoder() {
 //        return NoOpPasswordEncoder.getInstance();
 //    }
-//}
+    public ModelMapper modelMapper() {
+
+    }
+}
--- src/main/java/lk/ijse/pos/thogakadeapispring/service/impl/CustomerServiceIMPL.java
+++ src/main/java/lk/ijse/pos/thogakadeapispring/service/impl/CustomerServiceIMPL.java
@@ -1,6 +1,18 @@
 package lk.ijse.pos.thogakadeapispring.service.impl;
 
 import lk.ijse.pos.thogakadeapispring.dto.CustomerDTO;
+import lk.ijse.pos.thogakadeapispring.repo.CustomerRepo;
 import lk.ijse.pos.thogakadeapispring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class CustomerServiceIMPL implements CustomerService {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Override
+    public void saveCustomer(CustomerDTO dto) {
+
+    }
+}
 
