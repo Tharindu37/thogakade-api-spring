99b3c935140e8a3ef84ebb7b4e1a011b453cee67
Tharindu37 <tharindut520@gmail.com>
2022-09-18 10:20:39 +0530

    Create spring application

---
+++ src/main/java/lk/ijse/pos/thogakadeapispring/ThogakadeApiSpringApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.pos.thogakadeapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThogakadeApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThogakadeApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/pos/thogakadeapispring/ThogakadeApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.pos.thogakadeapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThogakadeApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
